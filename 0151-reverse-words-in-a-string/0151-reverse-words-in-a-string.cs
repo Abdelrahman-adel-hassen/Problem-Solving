fc5a461778c411548070578865239522f0177056
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2023-10-07 02:33:24 +0300

    Time: 73 ms (66.70%), Space: 38.5 MB (83.65%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,35 +1,9 @@
 public class Solution {
     public string ReverseWords(string s) {
-       
-        s=s.Trim();
-        int n=s.Length;
-        StringBuilder str=new StringBuilder(s);
-        StringBuilder res=new StringBuilder();
-        str=Reverse(0,n-1,str);
-        str.Append(' ');
-        int prevIndex=0;
-        for(int i=0;i<=n;i++){
-            if(i>0&&str[i]==' '&&str[i]==str[i-1]){
-               continue;
-            }
-            if(str[i]==' '){
-                res=Reverse(prevIndex,res.Length-1,res);
-                prevIndex=res.Length+1;
-            }
-            if(i!=n)
-             res.Append(str[i]);
-        }
-        return res.ToString();
-    }
-    private StringBuilder Reverse(int start,int end,StringBuilder s){
-      while(end>start){
-          char temp=s[start];
-          s[start]=s[end];
-          s[end]=temp;
-          end--;
-          start++;
-          
-      }
-        return s;
+        string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        
+        Array.Reverse(words);
+        
+        return string.Join(" ", words);
     }
 }
