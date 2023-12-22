b91b5b1f293ba40182f76dcdbb4cbcaf68ce8985
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2023-12-22 21:00:34 +0200

    Time: 61 ms (84.38%), Space: 44.3 MB (25.00%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cs
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cs
@@ -1,9 +1,5 @@
 public class Solution {
     public int MinPartitions(string n) {
-        int minimumNumberOfDeciBinary=0;
-        for(int i=0;i<n.Length;i++){
-            minimumNumberOfDeciBinary=Math.Max(minimumNumberOfDeciBinary,n[i]-'0');
-        }
-        return minimumNumberOfDeciBinary;
+        return n.Max()-'0';
     }
 }
