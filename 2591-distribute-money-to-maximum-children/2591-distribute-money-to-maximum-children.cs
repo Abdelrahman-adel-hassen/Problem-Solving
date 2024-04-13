6588be40ba9d782860fcdaafe6635d5aa2b46d28
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-04-13 09:16:59 +0200

    Time: 31 ms (22.22%), Space: 27.2 MB (72.22%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cs
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cs
@@ -6,7 +6,8 @@ public class Solution {
         money -= children;
         if (money < 0)
             return -1;
-        if (money / 7 == children && money % 7 == 0)
+        bool isNoMoreExist=money % 7 == 0;
+        if (money / 7 == children && isNoMoreExist)
             return children;
         if (money / 7 == children - 1 && money % 7 == 3)
             return children - 2;
