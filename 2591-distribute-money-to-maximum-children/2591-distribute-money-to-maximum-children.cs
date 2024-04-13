0a4e74c7f63c1e74c0636c31ae21940281066454
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-04-13 09:13:37 +0200

    Time: 24 ms (63.89%), Space: 27.3 MB (16.67%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cs
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cs
@@ -3,18 +3,13 @@ public class Solution {
         //divide money/children ==0 return false;
         //divide money/8 >children return children-1;
         //divide money/8 <children return money/8-1;
-        if(money/children==0)
+        money -= children;
+        if (money < 0)
             return -1;
-        int answer=0;
-        while(children>1&&((money-8)/(children-1))>0){
-            money-=8;
-            children--;
-            answer++;
-        }
-        if(children==1&&money==8)
-             answer++;
-        if(children==1&&money==4)
-             answer--;
-        return answer;
+        if (money / 7 == children && money % 7 == 0)
+            return children;
+        if (money / 7 == children - 1 && money % 7 == 3)
+            return children - 2;
+        return Math.Min(children - 1, money / 7);
     }
 }
