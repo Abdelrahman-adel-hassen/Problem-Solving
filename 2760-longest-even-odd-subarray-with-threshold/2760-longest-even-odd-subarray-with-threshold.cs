a957ece7a2dc3c342f12322e7c2fef0f34b82191
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-04-14 02:58:55 +0200

    Time: 151 ms (97.14%), Space: 69.9 MB (45.71%) - LeetHub

---
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int LongestAlternatingSubarray(int[] nums, int threshold) {
+          //if We found even and break the role restar ans=1
+
+        //even and ans%2==1 ans=1,even ans%2==0 ans++ 
+        //odd and  ans%2==0 ans=0, ans%2==1  ans++ 
+        int maxLongest=0,result=0;
+        for(int i=0;i<nums.Count();i++){
+            if(nums[i]>threshold){
+                maxLongest=0;
+                continue;
+            }
+            if(nums[i]%2==0)
+                maxLongest=maxLongest%2==1?1:maxLongest+1;
+            else
+                 maxLongest=maxLongest%2==0?0:maxLongest+1;
+            result=Math.Max(result,maxLongest);
+        }
+        return Math.Max(result,maxLongest);
+    }
+}
