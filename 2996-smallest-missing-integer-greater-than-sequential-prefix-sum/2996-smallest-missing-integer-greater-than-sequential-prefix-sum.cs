70eb98e1ecef3cb42caecf6cd40f9a5e8c904f74
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-04-14 04:42:07 +0200

    Time: 57 ms (83.58%), Space: 42.3 MB (23.88%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cs
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cs
@@ -4,18 +4,14 @@ public class Solution {
         return GetSmallestNumberMissigGreterThanSum(nums,maxNumberOfLongestSequentialPrefix);
     }
     private int GetSumOfLongestSequentialPrefix(int[] nums){
-        int maxNumberOfLongestSequentialPrefix=nums[0];
-        int maxLongestSequentialPrefix=1;
+        int sum=nums[0];
         for(int i=1;i<nums.Count();i++){
             if(nums[i]!=nums[i-1]+1)
                 break;
-            maxLongestSequentialPrefix++;
-           maxNumberOfLongestSequentialPrefix=nums[i];
+           sum+=nums[i];
         }
-        int total1=(maxNumberOfLongestSequentialPrefix*(maxNumberOfLongestSequentialPrefix+1))/2;
-        maxNumberOfLongestSequentialPrefix-=maxLongestSequentialPrefix;
-        int total2=(maxNumberOfLongestSequentialPrefix*(maxNumberOfLongestSequentialPrefix+1))/2;
-        return total1-total2;
+    
+        return sum;
     }
     private int GetSmallestNumberMissigGreterThanSum(int[]nums,int sum){
         var numbers=new Dictionary<int,bool>();
