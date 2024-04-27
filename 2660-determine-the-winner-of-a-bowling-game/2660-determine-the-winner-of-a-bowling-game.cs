c547e9226635b8c4e506241a3e70eb4dd5898177
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-04-27 18:53:38 +0300

    Time: 141 ms (38.10%), Space: 55.4 MB (61.90%) - LeetHub

---
+++ 2660-determine-the-winner-of-a-bowling-game/2660-determine-the-winner-of-a-bowling-game.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int IsWinner(int[] player1, int[] player2) {
+        
+        if(player1.Count()==1)
+            return GetTheWinner(player1[0],player2[0]);
+     
+        int score1=player1[0]+player1[1]*(player1[0]==10?2:1);
+        int score2=player2[0]+player2[1]*(player2[0]==10?2:1);
+        Console.WriteLine(score2+" "+score1);
+       for(int i=2;i<player1.Count();i++){
+          score1+=player1[i];
+          score2+=player2[i];
+          if(player1[i-1]==10||player1[i-2]==10)
+                  score1+=player1[i];
+          if(player2[i-1]==10||player2[i-2]==10)
+                  score2+=player2[i];
+
+       }
+
+       return GetTheWinner(score1,score2);
+    }
+    private int GetTheWinner(int score1,int score2){
+        if(score1==score2)
+            return 0;
+        return  score1>score2?1:2;
+    }
+}
