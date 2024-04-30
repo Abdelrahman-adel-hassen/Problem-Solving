b03264ed0e4525a1bf7c5b29d29851efd7833270
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-05-01 02:40:53 +0300

    Time: 13 ms (97.63%), Space: 27 MB (65.35%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MySqrt(int x) {
+        long l=0,r=x;
+        while(l<r){
+            long mid=(l+r+1)/2;
+            if(mid*mid>x)
+                r=mid-1;
+            else 
+                l=mid;
+        }
+        return (int)l;
+    }
+}
