ed927283a1f88ae23bbe45d5a8155e5b5c327ff4
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-05-03 22:24:08 +0300

    Time: 74 ms (23.03%), Space: 43.3 MB (36.18%) - LeetHub

---
+++ 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.cs
@@ -0,0 +1,51 @@
+public class Solution {
+    public int MinimumSubarrayLength(int[] nums, int k) {
+        if(k==0)
+            return 1;
+        int numbersWithORS=0;
+        int CurrentSubIndex=0;
+        int ans=51;
+        var bitWiseNumberCount=new int[32];
+        for(int i=0;i<nums.Count();i++){
+            SetBitWiseNumberCount(bitWiseNumberCount,nums[i]);
+            numbersWithORS=GetCurrentNumbersWithORS(bitWiseNumberCount);
+        
+            while(numbersWithORS>=k){
+
+                ans=Math.Min(ans,i-CurrentSubIndex+1);
+                UnSetBitWiseNumberCount(bitWiseNumberCount,nums[CurrentSubIndex]);
+                CurrentSubIndex++;
+                numbersWithORS=GetCurrentNumbersWithORS(bitWiseNumberCount);
+            }
+            
+               
+        }
+        return ans==51?-1:ans;
+    }
+    private void SetBitWiseNumberCount(int[] bitWiseNumberCount,int num){
+        int currentIndex=0;
+        while(num>0){
+            bitWiseNumberCount[currentIndex]+=(num&1)==1?1:0;
+            num>>=1;
+            currentIndex++;
+        }
+        
+    }
+    private void UnSetBitWiseNumberCount(int[] bitWiseNumberCount,int num){
+        int currentIndex=0;
+        while(num>0){
+            bitWiseNumberCount[currentIndex]-=(num&1)==1?1:0;
+            num>>=1;
+             currentIndex++;
+        }
+    }
+     private int GetCurrentNumbersWithORS(int[] bitWiseNumberCount){
+         int numbersWithORS=0;
+        for(int i=0;i<32;i++){
+            if(bitWiseNumberCount[i]>0){
+                numbersWithORS|=(1<<i);
+            }
+        }
+         return numbersWithORS;
+    }
+}
