562b9e570eb8a9bcbacf91bfa6a0f395f39e872c
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-05-31 04:47:27 +0300

    Time: 74 ms (5.09%), Space: 46.6 MB (6.78%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cs
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cs
@@ -1,10 +1,10 @@
 public class Solution {
     public bool CanBeEqual(string s1, string s2) {
-        s1=SortEvenOddIndicesUsingLinq(s1);
-        s2=SortEvenOddIndicesUsingLinq(s2);
+        s1=SortEvenOddIndices(s1);
+        s2=SortEvenOddIndices(s2);
         return s1==s2;
     }
-    private string SortEvenOddIndicesUsingLinq(string input)
+    private string SortEvenOddIndices(string input)
     {
         var evenChars = input.Where((c, i) => i % 2 == 0).OrderBy(c => c).ToArray();
         var oddChars = input.Where((c, i) => i % 2 != 0).OrderBy(c => c).ToArray();
