37bf88b844b2ee13d8319a341fd3bdfe04d4da1f
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-05-31 04:48:57 +0300

    Time: 2 ms (48.62%), Space: 8.2 MB (73.79%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,16 +1,14 @@
-public class Solution {
-    public bool CanBeEqual(string s1, string s2) {
-        s1=SortEvenOddIndices(s1);
-        s2=SortEvenOddIndices(s2);
-        return s1==s2;
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        if(s1 == s2) return true;
+        
+        if(s1[0] != s2[0]) swap(s1[0],s1[2]);
+        if(s1 == s2) return true;
+        
+        if(s1[1] != s2[1]) swap(s1[1],s1[3]);
+        if(s1 == s2) return true;
+        
+        return false;
     }
-    private string SortEvenOddIndices(string input)
-    {
-        var evenChars = input.Where((c, i) => i % 2 == 0).OrderBy(c => c).ToArray();
-        var oddChars = input.Where((c, i) => i % 2 != 0).OrderBy(c => c).ToArray();
-
-        return new string(input
-            .Select((c, i) => i % 2 == 0 ? evenChars[i / 2] : oddChars[i / 2])
-            .ToArray());
-    }
-}
+};
