3653960d33ed0b1ab2f67d79753b29c1959de0b7
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-06-21 18:55:59 +0300

    Time: 78 ms (10.34%), Space: 42.5 MB (44.83%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cs
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cs
@@ -9,10 +9,10 @@ public  bool HaveConflict(string[] event1, string[] event2)
     var event1EndTime = event1End[0] * 60 + event1End[1];
     var event2StartTime = event2Start[0] * 60 + event2Start[1];
     var event2EndTime = event2End[0] * 60 + event2End[1];
-    return IsIntersect(event1StartTime, event2StartTime, event2EndTime) || IsIntersect(event2StartTime, event1StartTime, event1EndTime);
+    return IsIntersect(event1StartTime,event1EndTime, event2StartTime, event2EndTime);
 }
-private bool IsIntersect(int event1StartTime, int event2StartTime, int event2EndTime)
+private bool IsIntersect(int event1StartTime,int event1EndTime, int event2StartTime, int event2EndTime)
 {
-    return event1StartTime <= event2EndTime && event1StartTime >= event2StartTime;
+    return event1StartTime <= event2EndTime && event1EndTime >= event2StartTime;
 }
 }
