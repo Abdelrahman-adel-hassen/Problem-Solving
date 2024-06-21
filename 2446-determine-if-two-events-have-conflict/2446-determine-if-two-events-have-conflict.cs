741cfdff0b7f735b3fa0f2fe9a2cfecf820d4ae9
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-06-21 18:47:23 +0300

    Time: 67 ms (51.72%), Space: 42.7 MB (24.14%) - LeetHub

---
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cs
@@ -0,0 +1,18 @@
+public class Solution {
+public  bool HaveConflict(string[] event1, string[] event2)
+{
+    var event1Start = event1[0].Split(':').Select(x => int.Parse(x)).ToList();
+    var event1End = event1[1].Split(':').Select(x => int.Parse(x)).ToList();
+    var event2Start = event2[0].Split(':').Select(x => int.Parse(x)).ToList();
+    var event2End = event2[1].Split(':').Select(x => int.Parse(x)).ToList();
+    var event1StartTime = event1Start[0] * 60 + event1Start[1];
+    var event1EndTime = event1End[0] * 60 + event1End[1];
+    var event2StartTime = event2Start[0] * 60 + event2Start[1];
+    var event2EndTime = event2End[0] * 60 + event2End[1];
+    return IsIntersect(event1StartTime, event2StartTime, event2EndTime) || IsIntersect(event2StartTime, event1StartTime, event1EndTime);
+}
+private bool IsIntersect(int event1StartTime, int event2StartTime, int event2EndTime)
+{
+    return event1StartTime <= event2EndTime && event1StartTime >= event2StartTime;
+}
+}
