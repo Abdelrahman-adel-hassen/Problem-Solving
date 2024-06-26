7ffc4e48c81ea87ab58547338dc95881b826f4f7
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-06-26 03:17:33 +0300

    Time: 24 ms (38.89%), Space: 26.8 MB (72.22%) - LeetHub

--- 3001-minimum-moves-to-capture-the-queen/3001-minimum-moves-to-capture-the-queen.cs
+++ 3001-minimum-moves-to-capture-the-queen/3001-minimum-moves-to-capture-the-queen.cs
@@ -1,13 +1,17 @@
 public class Solution {
     public int MinMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+        bool bishopIsBetweenQueenAndRockInColumn=(d>b&&d<f)||(d<b&&d>f);
+        bool bishopIsBetweenQueenAndRockInRow=(c>a&&c<e)||(c<a&&c>e);
+        bool bishopIsBetweenQueenAndRockInDiagonal1=(e>c&&e>a&&a>c)||(e<c&&e<a&&a<c);
+        bool bishopIsBetweenQueenAndRockInDiagonal2=(e>c&&e>a&&a>c)||(e<c&&e<a&&a<c);
         if(a==e)
-                return c==a&&((d>b&&d<f)||(d<b&&d>f))==true?2:1;
+                return (c==a&&bishopIsBetweenQueenAndRockInColumn)?2:1;
         else if(b==f)
-                return b==d&&((c>a&&c<e)||(c<a&&c>e))==true?2:1;  
+                return (b==d&&bishopIsBetweenQueenAndRockInRow)?2:1;  
         else if(c-d==e-f)     
-                    return c-d==a-b&&((e>c&&e>a&&a>c)||(e<c&&e<a&&a<c))==true?2:1;
+                    return (c-d==a-b&&bishopIsBetweenQueenAndRockInDiagonal1)?2:1;
         else if(c+d==e+f)
-            return c+d==a+b&&((e>c&&e>a&&a>c)||(e<c&&e<a&&a<c))==true?2:1;
+            return (c+d==a+b&&bishopIsBetweenQueenAndRockInDiagonal2)?2:1;
         return 2;
     }
 }
