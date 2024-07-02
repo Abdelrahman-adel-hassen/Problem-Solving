d05063a32a515c3a42cfc8f08f0bf426a3503a0f
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-07-02 08:49:22 +0300

    Time: 70 ms (100.00%), Space: 50 MB (27.59%) - LeetHub

---
+++ 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.cs
@@ -0,0 +1,66 @@
+public class Solution {
+    public bool IsItPossible(string word1, string word2)
+{
+        
+    if(word1 == word2)
+        return true;
+    var words1 = new int[26];
+    var words2 = new int[26];
+    foreach (var ch in word1)
+        words1[ch - 'a']++;
+    foreach (var ch in word2)
+        words2[ch - 'a']++;
+    int distinctOfWord1 = GetDistinctCharacters(words1);
+    int distinctOfWord2 = GetDistinctCharacters(words2);
+    if (Math.Abs(distinctOfWord1 - distinctOfWord2) > 2)
+        return false;
+    if(SameDistinctCharacters(words1 , words2))
+     return true;
+    for (int i = 0; i < 26; i++)
+    {
+        if (words1[i] == 0)
+            continue;
+
+        for (int j = 0; j < 26; j++)
+        {
+            if (words2[j] == 0)
+                  continue;
+              int newDistinctWord1 = distinctOfWord1;
+              if (i != j)
+              {
+                  if (words1[i] - 1 == 0)
+                      newDistinctWord1--;
+                  if (words1[j] + 1 == 1)
+                      newDistinctWord1++;
+              }
+
+              int newDistinctWord2 = distinctOfWord2;
+              if (i != j)
+              {
+                  if (words2[j] - 1 == 0)
+                      newDistinctWord2--;
+                  if (words2[i] + 1 == 1)
+                      newDistinctWord2++;
+              }
+            if (newDistinctWord1 != 0 && newDistinctWord2 == newDistinctWord1)
+                return true;
+        }
+    }
+    return false;
+}
+private int GetDistinctCharacters(int[] words)
+{
+    return words.Count(x => x > 0);
+}
+bool SameDistinctCharacters(int[] words1, int[] words2)
+{
+     for (int i = 0; i < 26; i++)
+ {
+     if (words1[i] == 0 && words2[i] == 0)
+         continue;
+     if (words1[i] == 0 || words2[i] == 0)
+         return false ;
+ }
+    return true;
+}   
+}
