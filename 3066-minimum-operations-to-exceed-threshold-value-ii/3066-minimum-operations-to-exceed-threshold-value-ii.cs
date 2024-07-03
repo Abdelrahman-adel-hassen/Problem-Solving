1f6a4c05e2bec383b335d62ed04da6e77a40b341
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-07-03 10:50:59 +0300

    Time: 746 ms (77.78%), Space: 78.8 MB (100.00%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cs
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.cs
@@ -1,25 +1,17 @@
 public class Solution {
     public int MinOperations(int[] nums, int k) {
-        int operations = 0;
-        var numbersGreaterOrEqualK = nums.Count(x => x >= k);
-        if (numbersGreaterOrEqualK == nums.Length)
-            return operations;
-
         var numbersSorted = new PriorityQueue<long, long>();
         foreach (int num in nums)
             numbersSorted.Enqueue(num, num);
 
-        while (numbersGreaterOrEqualK != numbersSorted.Count)
+        int operations = 0;
+        while (numbersSorted.Count > 0 && numbersSorted.Peek() < k)
         {
             long x = numbersSorted.Dequeue();
             long y = numbersSorted.Dequeue();
-            if (y >= k)
-                numbersGreaterOrEqualK--;
             operations++;
             long newValue = x * 2 + y;
             numbersSorted.Enqueue(newValue, newValue);
-            if (newValue >= k)
-                numbersGreaterOrEqualK++;
         }
         return operations;
     }
