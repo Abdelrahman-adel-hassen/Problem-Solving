03d3c53e33e6cc9b166d5636f3b455e343e7aed3
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-08-09 06:11:38 +0300

    Time: 150 ms (81.48%), Space: 78.6 MB (100.00%) - LeetHub

--- 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.cs
+++ 3040-maximum-number-of-operations-with-the-same-score-ii/3040-maximum-number-of-operations-with-the-same-score-ii.cs
@@ -12,11 +12,11 @@ public class Solution {
            return dp[st, en];
        int maxOperation = dp[st, en];
        if (score == -1 || score == nums[st] + nums[st + 1])
-           maxOperation = dp[st, en] = Solve(nums, st + 2, en, nums[st] + nums[st + 1], dp) + 1;
+           maxOperation = Solve(nums, st + 2, en, nums[st] + nums[st + 1], dp) + 1;
        if (score == -1 || score == nums[en] + nums[en - 1])
-           maxOperation = dp[st, en] = Math.Max(maxOperation, Solve(nums, st, en - 2, nums[en] + nums[en - 1], dp) + 1);
+           maxOperation = Math.Max(maxOperation, Solve(nums, st, en - 2, nums[en] + nums[en - 1], dp) + 1);
        if (score == -1 || score == nums[st] + nums[en])
-           maxOperation = dp[st, en] = Math.Max(maxOperation, Solve(nums, st + 1, en - 1, nums[st] + nums[en], dp) + 1);
+           maxOperation =  Math.Max(maxOperation, Solve(nums, st + 1, en - 1, nums[st] + nums[en], dp) + 1);
        return dp[st, en] = maxOperation;
     }
 }
