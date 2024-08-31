d1322ab86426b755721c4f4f6279435d1f2639ea
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-08-31 04:39:46 +0300

    Time: 66 ms (60.00%), Space: 46.2 MB (46.67%) - LeetHub

---
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public long MaxStrength(int[] nums) {
+        long ans = 1;
+        int maxNegativeNumber=-10;
+        int numZeros=0;
+        bool isPositve=false;
+        for(int i=0;i<nums.Length;i++){
+            if(nums[i]!=0){
+                 ans*=nums[i];
+                  if(ans>0||nums[i]>0)
+                     isPositve=true;
+            }
+            if(nums[i]<0)
+               maxNegativeNumber=Math.Max(maxNegativeNumber,nums[i]);
+            if(nums[i]==0) 
+                numZeros++;
+               
+        }
+        if(isPositve)
+            return  Math.Max(ans,ans/maxNegativeNumber);
+        if(numZeros==nums.Length)
+            return 0;
+       
+        return numZeros>0?0:ans;
+    }
+}
