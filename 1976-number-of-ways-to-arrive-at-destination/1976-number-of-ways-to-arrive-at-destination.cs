8df13a6c4cececcbb0b08b9d45182b83c7dbbc2d
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-09-04 19:10:26 +0300

    Time: 180 ms (37.04%), Space: 57.6 MB (100.00%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cs
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cs
@@ -2,15 +2,15 @@ public class Solution {
     public int CountPaths(int n, int[][] roads)
 {
     const long oo = long.MaxValue;
+    const long mod = 1000000007;
     var adj = Enumerable.Range(0, n)
                         .Select(_ => new List<(int cost, int destination)>())
                         .ToArray();
-    var roadCost = new long[n];
+    var roadCost = new (long cost, long ways)[n];
     var vis = new int[n];
-    var noOfWays = 0;
     var minMinutes = oo;
     for (int i = 0; i < n; i++)
-        roadCost[i] = oo;
+        roadCost[i] = (oo, 0);
 
     for (int i = 0; i < roads.Length; i++)
     {
@@ -20,13 +20,13 @@ public class Solution {
         adj[src].Add((cost, dest));
         adj[dest].Add((cost, src));
     }
-    roadCost[0] = 0;
+    roadCost[0] = (0, 1);
     var q = new PriorityQueue<(long cost, int destination), long>();
     q.Enqueue((0, 0), 0);
     while (q.Count > 0)
     {
         (long cost, int src) road = q.Dequeue();
-        if (road.cost != roadCost[road.src])
+        if (road.cost != roadCost[road.src].cost)
             continue;
 
         if (road.src == n - 1)
@@ -34,32 +34,18 @@ public class Solution {
 
         foreach (var neigb in adj[road.src])
         {
-            if (roadCost[neigb.destination] > road.cost + neigb.cost)
+            if (roadCost[neigb.destination].cost > road.cost + neigb.cost)
             {
-                roadCost[neigb.destination] = road.cost + neigb.cost;
-                q.Enqueue((roadCost[neigb.destination], neigb.destination), roadCost[neigb.destination]);
+                roadCost[neigb.destination].cost = road.cost + neigb.cost;
+                roadCost[neigb.destination].ways = roadCost[road.src].ways;
+                q.Enqueue((roadCost[neigb.destination].cost, neigb.destination), roadCost[neigb.destination].cost);
             }
+            else if (roadCost[neigb.destination].cost == road.cost + neigb.cost)
+                roadCost[neigb.destination].ways = (roadCost[neigb.destination].ways + roadCost[road.src].ways) % mod;
         }
     }
     Console.WriteLine("minutes " + minMinutes);
-    return DFS(0, n, 0, vis, roadCost, adj);
+    return (int)roadCost[n - 1].ways;
 
-}
-int DFS(int src, int n, long minCost, int[] vis, long[] roadCost, List<(int cost, int destination)>[] adj)
-{
-    if (minCost != roadCost[src])
-        return 0;
-    if (vis[src] != 0)
-        return vis[src];
-    if (src == n - 1)
-        return 1;
-    int result = 0;
-    int mod = 1000000007;
-    foreach (var neigb in adj[src])
-    {
-        result += DFS(neigb.destination, n, neigb.cost + minCost, vis, roadCost, adj);
-        result %= mod;
-    }
-    return vis[src] = result;
 }
 }
