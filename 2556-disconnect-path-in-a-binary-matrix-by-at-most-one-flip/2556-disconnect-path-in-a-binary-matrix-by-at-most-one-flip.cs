619433d3afa7a5b57764e409f5974b78e5ec19f2
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-09-06 08:13:47 +0300

    Time: 325 ms (100.00%), Space: 70.4 MB (44.44%) - LeetHub

--- 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cs
+++ 2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2556-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cs
@@ -7,16 +7,16 @@ public class Solution {
         if (n == 1 || m == 1)
             return n + m > 3;
 
-        return !NumberOfPaths(1, 0, n, m, grid) || !NumberOfPaths(0, 1, n, m, grid);
+        return !Solve(1, 0, n, m, grid) || !Solve(0, 1, n, m, grid);
     }
-    public bool NumberOfPaths(int i, int j, int n, int m, int[][] grid)
+    public bool Solve(int i, int j, int n, int m, int[][] grid)
     {
         if (i == n - 1 && j == m - 1)
             return true;
         if (i == n || j == m || grid[i][j] == 0)
             return false;
         grid[i][j] = 0;
-        return NumberOfPaths(i + 1, j, n, m, grid) || NumberOfPaths(i, j + 1, n, m, grid);
+        return Solve(i + 1, j, n, m, grid) || Solve(i, j + 1, n, m, grid);
     }
 
 }
