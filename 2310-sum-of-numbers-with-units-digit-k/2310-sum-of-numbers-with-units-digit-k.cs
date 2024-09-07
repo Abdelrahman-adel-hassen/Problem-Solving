e74770fc9f79f646e0b60667df354248dbad2e37
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-09-07 04:58:25 +0300

    Time: 20 ms (88.89%), Space: 26.7 MB (88.89%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cs
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cs
@@ -1,33 +1,9 @@
 public class Solution {
-    int[] dp = null;
-    public int MinimumNumbers(int num, int k)
+    public int MinimumNumbers(int sum, int k)
     {
-        if (num == 0)
-            return 0;
-        if(k==0)
-            return num%10==0?1:-1;
-        dp = new int[num + 1];
-        for (int i = 0; i <= num; i++)
-            dp[i] = -1;
-        int result = Solve(num, k);
-        return result >= 5000 ? -1 : result;
-    }
-
-    int Solve(int num, int k)
-    {
-        if (num == 0)
-            return 0;
-        
-        int result = 5000;
-        if (dp[num] != -1)
-            return dp[num];
-        for (int i = 0; i <= num / 10; i++)
-        {
-            int sum = (i * 10 + k);
-            if (sum > num)
-                break;
-            result = Math.Min(result, Solve(num - sum, k) + 1);
-        }
-        return dp[num] = result;
+      if (sum == 0) return 0;
+        for (int i = 1; i <= 10; ++i)
+            if ((i * k) % 10 == sum % 10 && i * k <= sum) return i;
+        return -1;
     }
 }
