627d9057ef16fd9aac3d55fabd3f68b899cdbbc5
Abdelrahman-adel-hassen <77240894+Abdelrahman-adel-hassen@users.noreply.github.com>
2024-09-26 04:18:56 +0300

    Time: 164 ms (62.96%), Space: 45.8 MB (100.00%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool FindSafeWalk(IList<IList<int>> grid, int health)
+    {
+        var vis = new bool[51, 51];
+        var dp = new short[51, 51, 101];
+        var isSafe = Solve(0, 0, grid, vis, dp, health);
+        return isSafe;
+    }
+    bool Solve(int i, int j, IList<IList<int>> grid, bool[,] vis, short[,,] dp, int health)
+    {
+        if (i < 0 || i == grid.Count || j < 0 || j == grid[0].Count)
+            return false;
+        if (grid[i][j] == 1)
+            health--;
+        if (health == 0)
+            return false;
+        if (i == grid.Count - 1 && j == grid[0].Count - 1)
+            return true;
+        if (vis[i, j])
+            return false;
+        if (dp[i, j, health] != 0)
+            return dp[i, j, health] == 1 ? true : false;
+        vis[i, j] = true;
+        var isSafe = Solve(i + 1, j, grid, vis, dp, health) || Solve(i, j + 1, grid, vis, dp, health) || Solve(i, j - 1, grid, vis, dp, health) || Solve(i - 1, j, grid, vis, dp, health);
+        vis[i, j] = false;
+        dp[i, j, health] = isSafe ? (short)1 : (short)2;
+        return isSafe;
+    }
+}
